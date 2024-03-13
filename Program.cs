c1a09a295f46f8bbd88a863c93fd36d681e6949e
ADMIN <ADMIN@DESKTOP-9VF6MN2>
2024-03-13 18:34:51 +1300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace AccessArrayForeach
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[] c = { 'A', 'B', 'C' };
+
+            Console.WriteLine("Element in the first index: " + c[0]);
+            Console.WriteLine("Element in the first index: " + c[1]);
+            Console.WriteLine("Element in the first index: " + c[2]);
+        }
+    }
+}
